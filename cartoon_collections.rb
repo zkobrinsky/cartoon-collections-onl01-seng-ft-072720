0cefacb51a82c8e21f9a930d7460aca8f3969b48
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 20:41:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.find{|name| name == cheese_types}
+  array.include?(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
