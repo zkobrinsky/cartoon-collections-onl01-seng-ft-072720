40f75819b1e135d94282253e56cdfbbcb293dea6
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 20:47:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,11 +15,11 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+
   if cheese_types.all? do |name| array.include?(i)
     return name
   end
 
   # array.any?(cheese_types)
-  
+
 end
