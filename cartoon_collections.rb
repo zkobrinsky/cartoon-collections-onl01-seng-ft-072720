6c78eaeb2c9b2d3ea2c7b2fe4e5c6c51ac22b9fd
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 20:04:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def long_planeteer_calls(array)
   array.each do |name|
     if name.length > 4
       return true
-    else
-      return false
+    # else
+    #   return false
     end
   end
 end
