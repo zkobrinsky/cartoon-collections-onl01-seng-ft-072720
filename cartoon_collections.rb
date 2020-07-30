35b929b77434ca16a0661a9333cb940cd9af8a05
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-30 20:01:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.each do |name|
+    if name.length > 4
+      true
+    end
 end
 
 def find_the_cheese# code an argument here
